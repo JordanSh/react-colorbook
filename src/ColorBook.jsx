9cbcc2d57a686e08b457f7b39d34ccde61861cc2
Yarden Shalom <yarden.shalom9@gmail.com>
2020-01-21 19:13:41 +0200

    better styling

--- src/ColorBook.jsx
+++ src/ColorBook.jsx
@@ -2,17 +2,17 @@ import React from "react"
 import styled from "styled-components"
 
 const ColorBook = ({colorsImport, colorsImportName}) => {
-    const colorsObject = {...colorsImport}
+    const colorsObj = {...colorsImport}
 
-    const mapColors = Object.keys(colorsObject).map((key,index)=>{
-        const colorCode = colorsObject[key]
+    const mapColors = Object.keys(colorsObj).map((key,index)=>{
+        const colorCode = colorsObj[key]
             return(
             <ColorDiv colorCode={colorCode} key={index}>
                 <KeyName >
                 {`\$\{${colorsImportName}.${key}\}`}
                 </KeyName>
                 <ColorName>
-                {colorsObject[key]}
+                {colorsObj[key]}
                 </ColorName>
             </ColorDiv>
             )
@@ -20,7 +20,14 @@ const ColorBook = ({colorsImport, colorsImportName}) => {
 
     return(
         <Wrapper>
+            <HeaderContainer>
+            <Header>My ColorBook<span>.</span></Header>
+            <Credits>by yarden shalom, check out my portfolio <a href="https://www.yardenshalom.com">👩‍🚀 yardenshalom.com</a></Credits>
+            </HeaderContainer>
+            <ColorsView>
             {mapColors}
+            </ColorsView>
+            
         </Wrapper>
     )
 }
@@ -30,12 +37,79 @@ export default ColorBook
 const Wrapper = styled.div`
 height: 100vh;
 width: 100vw;
+background: #0c5b6e;
+margin: 0 auto;
+display: flex;
+flex-direction: column;
+`
+
+const HeaderContainer = styled.div`
+flex-basis: 7%;
+height: 7%;
+display: flex;
+justify-content: flex-start;
+`
+
+const Header = styled.div`
+flex-basis: 30%;
+height: 100%;
+border-radius: 0 0 100px 0;
+background: #002129;
+color: white;
+display: flex;
+align-items: center;
+font-size: 2rem;
+font-family: 'Trebuchet MS', Helvetica, Arial, sans-serif;
+padding-left: 10px;
+
+span{
+    color:red;
+}
+
+@media (max-width:767px) {
+    font-size: 0.9rem;
+    padding-left: 5px;
+}
+`
+const Credits = styled.div`
+flex-basis: 40%;
+height: 100%;
+display: flex;
+align-items: flex-end;
+padding-bottom: 10px;
+font-size: 0.8rem;
+color: white;
+
+a{
+    color:white;
+    padding-left: 5px;
+}
+
+@media (max-width:767px) {
+    font-size: 0.5rem;
+}
+`
+const ColorsView = styled.div`
+flex-basis: 93%;
+height: 93%;
+width: 100%;
 background: white;
-margin: auto;
 display: flex;
 justify-content: flex-start;
 align-items: flex-start;
 flex-wrap: wrap;
+overflow-y: auto;
+
+    ::-webkit-scrollbar {
+  		width: 5px;
+	}
+	::-webkit-scrollbar-track {
+		background-color: darkgray;
+	}
+	::-webkit-scrollbar-thumb {
+  		background-color: #323232;
+  		outline: 1px solid slategrey;
+	}
 `
 
 const ColorDiv = styled.div`
